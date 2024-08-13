f85cdb4319fed296c7ce6c7bb9c09f32dd7aac4f
cianogrady <cianogrady@gmail.com>
2024-08-13 17:04:23 +0000

    Finals embedded Q10

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "mbed.h"
+
+DigitalIn button(PC_13);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+
+int main() {
+    // set state
+led1 = 1; // on
+led2 = 0;  //off 
+
+int flag = 0;
+
+while(true) {
+
+    if(button == 0) {
+        if(flag == 0) {
+
+            flag = 1;
+        led1 = !led1;
+        led2 = !led2;
+
+        wait_us(200000);
+        }
+    }
+    else {
+        flag = 0;
+
+    }
+}
+}
+
+
+
